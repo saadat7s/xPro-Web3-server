e2e33ad31797c9e67f1e1987e07baeaae52d46c6
saadat7s <saadatullah2002@gmail.com>
2025-12-15 21:02:32 +0500

    chore: remove old api/server.js (renamed to api/index.js)

--- api/server.js
+++
@@ -1,8 +0,0 @@
-// Vercel serverless function entry point
-// TypeScript compiles 'export default app' to 'module.exports.default = app'
-const serverModule = require('../dist/server.js');
-const app = serverModule.default || serverModule;
-
-// Export the Express app for Vercel
-module.exports = app;
-
